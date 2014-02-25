cce13ba55de756411d71962332a6a0d0d5c934bc
Jose Alejandro <jaleomp@gmail.com>
2014-02-25 19:18:38 +0100

    Versión 1: Se cambia el estado a estable. Parece que no hay fallos. Se ha sustituído un método deprecado (session_is_loggedinas()  -> \core\session\manager::is_loggedinas()) Se introduce un consejo de sincronizar cadenas con AMOS. Cambiada la fecha de la versión.

--- theme/cafe_con_leche/version.php
+++ theme/cafe_con_leche/version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014020401; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2014022500; // The current module version (Date: YYYYMMDDXX)
 $plugin->release   = "1";
 $plugin->requires  = 2013040500; // Requires this Moodle version (2.5)
 $plugin->component = 'theme_cafe_con_leche';
